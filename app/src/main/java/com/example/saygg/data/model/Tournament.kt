495bfb40a2c0f6d4da2730bcc57216bcd6bcd05f
GabrielGomezGG <43209264+GabrielGomezGG@users.noreply.github.com>
2023-05-04 15:45:43 -0300

    change the query TournamentsByCountry

--- app/src/main/java/com/example/saygg/data/model/Tournament.kt
+++ app/src/main/java/com/example/saygg/data/model/Tournament.kt
@@ -1,8 +1,11 @@
 package com.example.saygg.data.model
 
 data class Tournament(
-    val name : String,
-    val startAt : Int,
-    val endAt : Int,
+    val images: List<Image>,
+    val name: String,
+    val startAt: Int,
+    val endAt: Int,
+    val numAttendees: Int = 0,
+    val events: List<Event>,
     val rules : String
 )
