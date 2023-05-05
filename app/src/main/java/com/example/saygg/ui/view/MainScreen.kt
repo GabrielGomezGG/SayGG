8038e592ab1935a0af2976718c85986a2d3432ad
GabrielGomezGG <43209264+GabrielGomezGG@users.noreply.github.com>
2023-05-05 17:39:23 -0300

    implement ModalNavigationDrawer, ModalDrawerSheet and NavigationDrawerItem

--- app/src/main/java/com/example/saygg/ui/view/MainScreen.kt
+++ app/src/main/java/com/example/saygg/ui/view/MainScreen.kt
@@ -2,12 +2,18 @@ package com.example.saygg.ui.view
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Divider
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -15,6 +21,7 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.NavigationDrawerItemColors
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -24,6 +31,8 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -47,12 +56,47 @@ fun MainScreen(tournamentViewModel: TournamentViewModel) {
         drawerState = drawerState,
         drawerContent = {
             ModalDrawerSheet(
-
-            ) {
+                Modifier.width(80.dp)
+            ){
                 NavigationDrawerItem(
-                    label = { Text(text = "titi") },
-                    selected = true,
-                    onClick = { /*TODO*/ })
+                    icon = {
+                        Image(
+                            painter = painterResource(id = R.drawable.startgglogo),
+                            contentDescription = null,
+                        )
+                    },
+                    label = {
+                    },
+                    selected = false,
+                    onClick = { /*TODO*/ },
+                )
+                Divider(thickness = 3.dp)
+                NavigationDrawerItem(
+                    icon = {
+                        Icon(
+                            imageVector = Icons.Default.Search,
+                            contentDescription = null,
+                            Modifier.fillMaxSize()
+                        )
+                    },
+                    label = {
+                    },
+                    selected = false,
+                    onClick = { /*TODO*/ },
+                )
+                NavigationDrawerItem(
+                    icon = {
+                        Icon(
+                            imageVector = Icons.Default.Notifications,
+                            contentDescription = null,
+                            Modifier.fillMaxSize()
+                        )
+                    },
+                    label = {
+                    },
+                    selected = false,
+                    onClick = { /*TODO*/ },
+                )
             }
         },
     ) {
