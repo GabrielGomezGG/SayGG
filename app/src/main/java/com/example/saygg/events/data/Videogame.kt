6f9785b4536f34b29418305bf0ad9611e7665d94
GabrielGomezGG <43209264+GabrielGomezGG@users.noreply.github.com>
2023-05-29 21:37:53 -0300

    implement: - EventScreens - new String "View Event" refactor: - Class Event, VideoGame now is in the package "event"

---
+++ app/src/main/java/com/example/saygg/events/data/Videogame.kt
@@ -0,0 +1,8 @@
+package com.example.saygg.events.data
+
+import com.example.saygg.tournament.data.model.Image
+
+data class VideoGame(
+    val name: String,
+    val images: List<Image>,
+)
--- app/src/main/java/com/example/saygg/tournament/data/model/Videogame.kt
+++
@@ -1,6 +0,0 @@
-package com.example.saygg.tournament.data.model
-
-data class VideoGame(
-    val name: String,
-    val images: List<Image>,
-)
